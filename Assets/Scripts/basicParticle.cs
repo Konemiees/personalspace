d662cdec17478da519c01e58dcfd8efd3f8fff29
Konemiees <konsta.sinisalo@gmail.com>
2015-01-19 11:38:52 +0200

    Eka commit Space Impact -kloonille.

---
+++ Assets/Scripts/basicParticle.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class basicParticleScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
