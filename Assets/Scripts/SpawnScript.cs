05444276b7dfb963d88bb5c007081744f2f726dc
Konemiees <konsta.sinisalo@gmail.com>
2015-01-21 17:26:11 +0200

    SpawnScript aluke

--- Assets/Scripts/SpawnScript.cs
+++ Assets/Scripts/SpawnScript.cs
@@ -3,13 +3,51 @@ using System.Collections;
 
 public class SpawnScript : MonoBehaviour {
 
-	// Use this for initialization
+	public float minSpawnTime = 0.2f;
+
+	private float spawnTime;
+	private float timePassed;
+
+	//Testaamiseen
+	private float testScore;
+	private float delta;
+
+
+
+
 	void Start () {
-	
+		testScore = 0;
+		timePassed = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+		//Testaamiseen
+		testScorer();
+
+
+		if (timePassed == 0) {
+			newSpawnTime();
+		}
+
+		timePassed += Time.deltaTime;
+
+		if (timePassed > spawnTime) {
+
+		}
+		
 	}
+
+
+
+	//Testaamiseen
+	private void testScorer(){
+		testScore += Time.deltaTime * 40;
+	}
+
+	private void newSpawnTime(){
+
+	}
+
 }
--- Assets/Scripts/basicParticle.cs
+++ Assets/Scripts/basicParticle.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class basicParticle : Entity {
 	
 	protected float endPoint = -24;
+	public int points = 100;
 
 
 	void Update () {
