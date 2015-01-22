cc19565d6329ad55227b5e336fd8084795e102e2
Esa Aaltonen <estaalt@utu.fi>
2015-01-22 15:47:57 +0200

    Scrollaava tausta. vähän sekava

---
+++ Assets/Scripts/SpaceBackground.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine; 
+using System.Collections;
+
+public class SpaceBackground : MonoBehaviour { 
+
+	public float speed = 0;
+
+	void Update() {
+		renderer.material.mainTextureOffset = new Vector2 (Time.time * speed, 0f);
+	}
+}
