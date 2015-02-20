f1ec5af16f957ca55cc0604f455a81c5e6a8989c
Konemiees <konsta.sinisalo@gmail.com>
2015-02-20 14:43:41 +0200

    mergecommit

---
+++ Assets/Scripts/GameEntities/Spartan.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spartan : basicParticle {
+
+	public Transform target;
+
+
+	void Start () {
+	
+	}
+	
+
+	void Update () {
+		paused = FindObjectOfType<Menuscript>().paused;
+		
+		if(transform.position.x > endPoint){
+			
+		} else {
+			die ();
+		}
+	}
+}
--- Assets/Scripts/SpaceBackground.cs
+++ Assets/Scripts/SpaceBackground.cs
@@ -5,11 +5,14 @@ public class SpaceBackground: MonoBehaviour {
 
 	public float speed = 0;
 	public int paused = -1;
+	private float delta = 0;
+	private float fixedPoint;
 
 	void Update() {
 		paused = FindObjectOfType<Menuscript>().paused;
 		if (paused == -1) {
-			renderer.material.mainTextureOffset = new Vector2 (Time.time * speed, 0f);
-		}
+			fixedPoint += Time.deltaTime;
+			renderer.material.mainTextureOffset = new Vector2 (fixedPoint * speed, 0f);
+		} 
 	}
 }
