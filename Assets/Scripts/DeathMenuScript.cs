a48dfea68e5994dbdece2d2bd24f5839d358b64e
Esa A <estaalt@utu.fi>
2015-02-28 19:25:56 +0200

    Highscoret ja kuoloruutu

--- Assets/Scripts/DeathMenuScript.cs
+++ Assets/Scripts/DeathMenuScript.cs
@@ -1,24 +1,30 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class DeathMenuScript : MonoBehaviour {
 
 	private Player player;
+
+	private Text scoreText;
 	//private bool hasDied;
 
 	// Use this for initialization
 	void Start () {
 
 		player = GameObject.FindGameObjectsWithTag ("Player") [0].GetComponent<Player> ();
+		scoreText = GameObject.Find ("death_score").GetComponent<Text> ();
 
-		renderer.enabled = false;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+		scoreText.text = "Score: " + player.Score;
+
 		if (player.died) {
-			renderer.enabled = true;
+
 		}
 
 
---
+++ Assets/Scripts/SendHighscore.js
@@ -0,0 +1,48 @@
+﻿#pragma strict
+
+var getURL = "http://192.168.1.37/:3000/highscore";
+var postURL = "http://192.168.1.37/:3000/score";
+
+
+
+//var button : UnityEngine.UI.Button;
+
+function Start() {
+
+	//button.GetComponent(UI.Button).onClick.AddListener(ButtonClicked);
+}
+
+function ButtonClicked(){
+	var name = "Matti";
+	//name = GameObject.Find("highscore").GetComponent(InputField).Text;
+	var score = "100";
+
+	UploadHighscore(name, score);
+}
+
+function UploadHighscore(name, score){
+	var form = new WWWForm();
+	form.AddField("name", name.ToString());
+	form.AddField("score", score.ToString());
+
+	var w = WWW(postURL, form);
+	yield w;
+
+	if(!String.IsNullOrEmpty(w.error)){
+		print(w.error);
+	}else{
+		print("Finished uploading highscore");
+	};
+}
+
+function DownloadHighscore(){
+	var highscore = new WWW(getURL);
+	/*Wait for data to download before releasing contact*/
+	yield highscore;
+	if(highscore.error == null){
+		Debug.Log("Download completed.");
+	}else{
+		Debug.Log("www-error: " + highscore.error);
+	}
+	Debug.Log( highscore.text);
+}
