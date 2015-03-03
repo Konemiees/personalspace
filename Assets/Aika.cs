5e732a8a5eb56babff7e456055a11e12b8ca86e3
evatap <evatap@utu.fi>
2015-03-04 00:32:00 +0200

    intro

---
+++ Assets/Aika.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Aika : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > 78) {
+			Application.LoadLevel ("mainMenu");
+		}
+	
+	}
+}
---
+++ Assets/Skipbutton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Skipbutton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		OnGUI ();
+		
+	}
+
+	void OnGUI(){
+		//Here if the player hit the button Start will trigger an action
+		if(GUI.Button(new Rect(8*Screen.width/9, 10*Screen.height/11, 100, 25), "Skip Intro")){
+			//Here we call the method who will call the level named "LevelName"
+			Application.LoadLevel("MainMenu");
+		}
+	}
+}
